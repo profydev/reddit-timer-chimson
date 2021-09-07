54baa03b00985098be8337d1f3d38bf6eb79fd9b
Chimson <crunor55@hotmail.com>
2021-09-07 17:49:35 +0200

    moved BrowserRouter to App component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Route, Switch, Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './index.css';
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={() => <div>/</div>} />
-        <Route exact path="/search" component={() => <div>search</div>} />
-      </Switch>
+      <div>
+        <Switch>
+          <Route exact path="/" component={() => <div>/</div>} />
+          <Route exact path="/search" component={() => <div>search</div>} />
+        </Switch>
+      </div>
     </Router>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 // eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <App />,
+
   document.getElementById('root'),
 );
