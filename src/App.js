a0a8b6e1594a7c1df9ba49f4f5e8a5fc9750b5d9
chimson <crunor55@hotmail.com>
2021-09-07 17:36:04 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Router } from 'react-router-dom';
 import './index.css';
 
 function App() {
   return (
-    <div>
+    <Router>
       <Switch>
         <Route exact path="/" component={() => <div>/</div>} />
         <Route exact path="/search" component={() => <div>search</div>} />
       </Switch>
-    </div>
+    </Router>
   );
 }
 
