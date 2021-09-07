339aae70ee16577330cfd1b681e7e6363319a196
Chimson <crunor55@hotmail.com>
2021-09-07 17:28:25 +0200

    fixed eslint quote rule

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import "./index.css";
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import './index.css';
 
 function App() {
   return (
