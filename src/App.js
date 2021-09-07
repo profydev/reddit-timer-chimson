5fc4a69a353167dd9e1476c0502bc5ef88f9b4aa
Chimson <crunor55@hotmail.com>
2021-09-07 17:13:25 +0200

    reset default browser styles with normalize.css, installed Bitter and Montserrat fonts, added index.css file for global styles, added react-router with / and /search routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./index.css";
 
 function App() {
   return (
     <div>
-      App Placeholder
+      <Switch>
+        <Route exact path="/" component={() => <div>/</div>} />
+        <Route exact path="/search" component={() => <div>search</div>} />
+      </Switch>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { render } from "@testing-library/react";
+import React from "react";
+import App from "./App";
 
-test('renders App', () => {
+test("renders App", () => {
   render(<App />);
 });
