b88f468f7b0fc1cdd57fefd0fabab1bd90b09ef8
Chimson <crunor55@hotmail.com>
2021-09-08 17:08:12 +0200

    Change app title and meta description

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import { Normalize } from 'styled-normalize';
 import GlobalStyles from './GlobalStyles';
+import theme from './theme';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Normalize />
-        <GlobalStyles />
-        App Placeholder
-        <Switch>
-          <Route path="/" exact>
-            Home
-          </Route>
-          <Route path="/search" exact>
-            Search
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <div>
+          <Normalize />
+          <GlobalStyles />
+          App Placeholder
+          <Switch>
+            <Route path="/search">Search</Route>
+            <Route path="/">Home</Route>
+          </Switch>
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
 
