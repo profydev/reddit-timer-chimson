8e4054b3c91a60a9424ca4b62d400ecf9c14bfde
chimson <crunor55@hotmail.com>
2021-09-08 17:33:19 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import Header from './components/Header';
 import { Normalize } from 'styled-normalize';
 import GlobalStyles from './GlobalStyles';
 import theme from './theme';
+
 
 function App() {
   return (
