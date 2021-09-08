17e435d624b2d9faf2e646d22f579b0de6b10de4
Chimson <crunor55@hotmail.com>
2021-09-08 16:25:52 +0200

    added Header component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Normalize } from 'styled-normalize';
+import Header from './components/Header';
 import GlobalStyles from './GlobalStyles';
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
       <div>
         <Normalize />
         <GlobalStyles />
-        App Placeholder
+        <Header />
         <Switch>
           <Route path="/" exact>
             Home
