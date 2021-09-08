b1baa32e01822ba7eaa060e9bee5132bfa987d02
Chimson <crunor55@hotmail.com>
2021-09-08 13:49:50 +0200

    add Router with / and /search routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Normalize } from 'styled-normalize';
 import GlobalStyles from './GlobalStyles';
 
 function App() {
   return (
-    <div>
-      <Normalize />
-      <GlobalStyles />
-      App Placeholder
-    </div>
+    <Router>
+      <div>
+        <Normalize />
+        <GlobalStyles />
+        App Placeholder
+        <Switch>
+          <Route path="/" exact>
+            Home
+          </Route>
+          <Route path="/search" exact>
+            Search
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
